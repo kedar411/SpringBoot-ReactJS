595b1faf0743d3247ece13890024fa15e807116f
Kedar <kedar411@yahoo.com>
2016-05-31 20:00:12 +0530

    Changes

--- springbootwebapp-springboot-mysql/src/main/java/guru/springframework/configuration/SecurityConfiguration.java
+++ springbootwebapp-springboot-mysql/src/main/java/guru/springframework/configuration/SecurityConfiguration.java
@@ -11,7 +11,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
-                .authorizeRequests().antMatchers("/console/**").permitAll();
+                .authorizeRequests().antMatchers("/cownsole/**").permitAll();
 
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
