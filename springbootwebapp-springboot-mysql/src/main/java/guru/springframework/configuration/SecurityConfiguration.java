0a5df1e7c199ce74eec27ffed43bd01b3d6356cd
Kedar <kedar411@yahoo.com>
2016-05-31 20:18:07 +0530

    Change 2

--- springbootwebapp-springboot-mysql/src/main/java/guru/springframework/configuration/SecurityConfiguration.java
+++ springbootwebapp-springboot-mysql/src/main/java/guru/springframework/configuration/SecurityConfiguration.java
@@ -16,5 +16,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
     }
-
 }
